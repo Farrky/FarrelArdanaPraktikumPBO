5a217263e8f11372ee6997c99b00d17d1b622270
Farrky <129332980+Farrky@users.noreply.github.com>
2024-02-26 11:47:27 +0700

    Update MOperasiTitik.java

--- Praktikum 1/MOperasiTitik.java
+++ Praktikum 1/MOperasiTitik.java
@@ -1,7 +1,7 @@
 /* Program   : MOperasiTitik.java */
 /* Deskripsi : File modul OperasiTitik */
 /* NIM & Nama  : 24060122140165 & Farrel Ardana Jati */
-/* Tanggal   : 16 Februari 2024 *
+/* Tanggal   : 16 Februari 2024 */
 public class MOperasiTitik{	
 	public static void main(String[] args){	
 		Titik t = new Titik(4,4);
