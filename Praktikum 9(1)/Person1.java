a87ab4679249cea0ef85acb3084f8e7b3f82d70b
Farrky <129332980+Farrky@users.noreply.github.com>
2024-05-14 10:29:58 +0700

    Add files via upload

---
+++ Praktikum 9(1)/Person1.java
@@ -0,0 +1,16 @@
+/**
+ * File : SerializePerson.java
+ * Deskripsi : Program untuk serialisasi objek Person
+ */
+ 
+ import java.io.*;
+ // class Person
+ class Person1 implements Serializable{
+	private String name;
+	public Person(String n){
+		name = n;
+	}
+	public String getName(){
+		return name;
+	}
+ }
