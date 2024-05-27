f599c6675954b9d7bd7514d888284a6ce6f6a8f0
Farrky <129332980+Farrky@users.noreply.github.com>
2024-05-27 11:31:43 +0700

    Add files via upload

---
+++ Praktikum 11/LambdaList.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum.pkg11;
+import java.util.ArrayList;
+/**
+ *
+ * @author LENOVO LEGION
+ */
+public class LambdaList {
+    public static void main(String[] args){
+        ArrayList<String> mahasiswaList = new ArrayList<>();
+        mahasiswaList.add("Adi");
+        mahasiswaList.add("Bambang");
+        mahasiswaList.add("Cici");
+        mahasiswaList.add("Cecep");
+        
+        mahasiswaList.forEach((nama) -> System.out.println(nama));
+    }
+    
+}
